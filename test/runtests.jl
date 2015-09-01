f4d75a0f285fc7c3bcb712ee3456d5e43eb29d01
Michael Francis <michael.francis@blackrock.com>
2015-09-01 14:26:21 -0400

    Fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,7 +16,7 @@ using Base.Test
 @test last( @NT( a => 1, b => "hello", c => 2.0 )) == ( :c, 2.0)
 @test [ v for( (k,v) in @NT( a => 1.0, b => 2.0 ) ) ] == [ 1.0, 2.0 ]
 
-@test ( x = @NT( a::Int64, b::Float64 )( 1, 2.0 ) ; fieldtype( x, :a ) == Int64 && fieldtype( x, :b) == Float64 )
+@test ( x = @NT( a::Int64, b::Float64 )( 1, 2.0 ) ; fieldtype( typeof(x), :a ) == Int64 && fieldtype( typeof(x), :b) == Float64 )
 @test @NT( a => 1, b => "hello")  ==  @NT( a, b )( 1, "hello")
 @test @NT( a => 1) != @NT( b => 1 )
 
@@ -38,9 +38,9 @@ using Base.Test
 @test isbits( @NT( ::Int64, ::Float64)) == true
 
 nt = @NT( a=>1, b=>2, c=>3 )
-@test x.a == 1
-@test x.b == 2
-@test x.c == 3
+@test nt.a == 1
+@test nt.b == 2
+@test nt.c == 3
 @test haskey( nt, x ) == false
 x = setindex( nt, :x, 123 )
 @test x.x == 123
